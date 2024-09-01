3d3dd329870a1df311851e407d5cd5ed6c8bb3c0
Yury <yury.s.nazarov@yandex.ru>
2024-09-01 22:30:36 +0300

    добавили urls

---
+++ mysite/blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+# Пространство имен позволяет обращатся к url по схеме: {app_name:name}
+app_name = 'blog'
+
+# Django просматривает все url и останавливается на том,
+# который первый подходит под шаблон
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+]
+
+
+
+# Docs
+# path & re_path (https://docs.djangoproject.com/en/5.1/ref/urls/#django.urls.re_path) для определяния шаблонов url-адресов
+# regexp: https://docs.python.org/3/howto/regex.html
+# Все возможные конверторы путей можно посмотреть тут https://docs.djangoproject.com/en/5.1/topics/http/urls/#path-converters
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog'))
 ]
+
+# Docs
+# URL Namespace: https://docs.djangoproject.com/en/4.1/topics/http/urls/#url-namespaces
