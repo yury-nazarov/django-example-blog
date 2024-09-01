5c3b91330014723192214bd6c5e50ef94a838d65
Yury <yury.s.nazarov@yandex.ru>
2024-09-01 21:55:06 +0300

    стандартный view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import Http404
 # Create your views here.
+
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'post': posts}
+                  )
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post}
+                  )
+
