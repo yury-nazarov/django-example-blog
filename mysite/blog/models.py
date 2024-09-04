5eecfd50b41fdc8942a3fcf5bf7a4fea36a5860d
Yury <yury.s.nazarov@yandex.ru>
2024-09-04 09:42:11 +0300

    Канонические url адреса для постов блога

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -23,7 +24,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    # Не может быть два одинаковых слага с одной датой публикации
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
 
@@ -31,7 +33,7 @@ class Post(models.Model):
 
     # timezone.now - вернет дату и время в формате зависящем от часового пояса
     publish = models.DateTimeField(default=timezone.now)
-    # ДАта и время создания поста
+    # Дата и время создания поста
     created = models.DateTimeField(auto_now_add=True)
     # последняя дата обновления поста
     # auto_now - обновляет дату и время автоматически
@@ -48,4 +50,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """
+        :return:  Канонический адрес объекта
+        """
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,7 +8,9 @@ app_name = 'blog'
 # который первый подходит под шаблон
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>',
+         views.post_detail,
+         name='post_detail'),
 ]
 
 
